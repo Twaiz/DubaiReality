311f3a26098a11a8505fab6090d84664c108eb1f
L-Twaiz <homenkobodya@gmail.com>
2024-02-29 20:11:25 +0200

    Summary (required)

---
+++ src/Js/lazy-load-section.js
@@ -0,0 +1,13 @@
+function onEntry(entry) {
+  entry.forEach((change) => {
+    if (change.isIntersecting) {
+      change.target.classList.add("el-scroll-show");
+    }
+  });
+}
+let options = { threshold: [0.2] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll(".el-scroll-hidden");
+for (let elm of elements) {
+  observer.observe(elm);
+}
