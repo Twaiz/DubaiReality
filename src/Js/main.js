28e769eb5abd3612e1f8b132f102b8b1803ff768
L-Twaiz <homenkobodya@gmail.com>
2024-03-10 15:45:23 +0200

    Summary (required)

---
+++ src/Js/main.js
@@ -0,0 +1,108 @@
+// menu icon
+
+const refs = {
+  openModalBtn: document.querySelector('[data-action="open-modal"]'),
+  closeModalBtn: document.querySelector('[data-action="close-modal"]'),
+  backdrop: document.querySelector(".backdrop"),
+  menuIcon: document.querySelector(".menu-icon"),
+
+  openModalBtnHero: document.querySelector(
+    '[data-action="hero-modal-open-btn"]'
+  ),
+  closeModalBtnHero: document.querySelector(
+    '[data-action="hero-modal-close-btn"]'
+  ),
+  backdropHero: document.querySelector(".hero-backdrop"),
+};
+
+refs.openModalBtn.addEventListener("click", onOpenModal);
+refs.closeModalBtn.addEventListener("click", onCloseModal);
+refs.backdrop.addEventListener("click", onBackdropClick);
+
+refs.openModalBtnHero.addEventListener("click", onOpenModalHero);
+refs.closeModalBtnHero.addEventListener("click", onCloseModalHero);
+refs.backdropHero.addEventListener("click", onBackdropClickHero);
+
+function onOpenModal() {
+  window.addEventListener("keydown", onEscKeyPress);
+  document.body.classList.add("show-modal");
+}
+
+function onCloseModal() {
+  window.removeEventListener("keydown", onEscKeyPress);
+  document.body.classList.remove("show-modal");
+}
+
+function onBackdropClick(event) {
+  if (event.currentTarget === event.target) {
+    console.log("Кликнули именно в бекдроп!!!!");
+    onCloseModal();
+  }
+}
+
+function onEscKeyPress(event) {
+  const ESC_KEY_CODE = "Escape";
+  const isEscKey = event.code === ESC_KEY_CODE;
+
+  if (isEscKey) {
+    onCloseModal();
+  }
+}
+
+// HERO
+
+function onOpenModalHero() {
+  window.addEventListener("keydown", onEscKeyPressHero);
+  document.body.classList.add("hero-show-modal");
+}
+
+function onCloseModalHero() {
+  window.removeEventListener("keydown", onEscKeyPressHero);
+  document.body.classList.remove("hero-show-modal");
+}
+
+function onBackdropClickHero(event) {
+  if (event.currentTarget === event.target) {
+    console.log("Кликнули именно в бекдроп!!!!");
+    onCloseModalHero();
+  }
+}
+
+function onEscKeyPressHero(event) {
+  const ESC_KEY_CODE = "Escape";
+  const isEscKey = event.code === ESC_KEY_CODE;
+
+  if (isEscKey) {
+    onCloseModalHero();
+  }
+}
+
+// SLIDER
+
+new Swiper(".swiper-container", {
+  // Стрелки
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  // Навигация
+  // Булеты, текущее положение, прогресбар
+  pagination: {
+    el: ".swiper-pagination",
+    // Булеты
+    type: 'bullets',
+    clickable: true,
+    // Динамические булеты
+    dynamicBullets: true,
+    // Фракция
+    // type: 'fraction',
+    // Прогрессбар
+    // type: 'progressbar'
+    // Скролл
+  },
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//     // Возможность перетаскивать скролл
+//     draggable: true 
+//   },
+});
