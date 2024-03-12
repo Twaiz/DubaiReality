816423f38fed717d0d07d38d440d897f8db300ea
L-Twaiz <homenkobodya@gmail.com>
2024-03-12 23:29:58 +0200

    Update main.js (footer, refs)

--- src/Js/main.js
+++ src/Js/main.js
@@ -16,6 +16,9 @@ const refs = {
 
   FaqBtn: document.querySelectorAll(".FAQ-button"),
   FaqDescr: document.querySelector(".FAQ-description"),
+
+  FooterBtn: document.querySelectorAll(".footer-btn"),
+  FooterLink: document.querySelectorAll(".footer-link"),
 };
 
 refs.openModalBtn.addEventListener("click", onOpenModal);
@@ -95,30 +98,15 @@ new Swiper(".image-slider", {
 
 // FAQ
 
-// refs.FaqBtn.forEach(btn => {
-//   btn.addEventListener("click", onShowDescription);
-// });
-
-// function onShowDescription(event) {
-//   const parent = event.target.parentElement;
-//   const description = parent.querySelector(".FAQ-description");
-
-//   if (description.style.display === "block") {
-//     description.style.display = "none";
-//   } else {
-//     description.style.display = "block";
-//   }
-// }
-
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   var faqButtons = document.querySelectorAll(".FAQ-button");
 
-  faqButtons.forEach(function(button) {
-    button.addEventListener("click", function() {
+  faqButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
       var description = button.nextElementSibling;
       var allDescriptions = document.querySelectorAll(".FAQ-description");
 
-      allDescriptions.forEach(function(desc) {
+      allDescriptions.forEach(function (desc) {
         if (desc !== description) {
           desc.style.display = "none";
         }
@@ -132,5 +120,54 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
 });
+
+// FOOTER
+
+const footerBtns = document.querySelectorAll(".footer-btn");
+const footerLists = document.querySelectorAll(".footer-list");
+let activeBtn = null;
+
+footerBtns.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    const linksToShow = document.querySelectorAll(
+      `.footer-list:nth-child(${index + 1}) .footer-link`
+    );
+
+    // Если нажата та же самая кнопка, скрываем ссылки и убираем класс активности
+    if (activeBtn === btn) {
+      linksToShow.forEach((link) => {
+        link.style.display = "none";
+      });
+      btn.classList.remove("active");
+      activeBtn = null;
+    } else {
+      // Убираем класс активности с предыдущей кнопки и скрываем соответствующие ссылки
+      if (activeBtn) {
+        activeBtn.classList.remove("active");
+        footerLists.forEach((list) => {
+          list.querySelectorAll(".footer-link").forEach((link) => {
+            link.style.display = "none";
+          });
+        });
+      }
+
+      // Показываем ссылки для текущей кнопки и устанавливаем ее как активную
+      linksToShow.forEach((link) => {
+        link.style.display = "block";
+      });
+      btn.classList.add("active");
+      activeBtn = btn;
+    }
+  });
+});
+
+// Показываем все ссылки при загрузке страницы, если ширина окна больше или равна 1330 пикселей
+if (window.innerWidth >= 1330) {
+  footerLists.forEach((list) => {
+    list.querySelectorAll(".footer-link").forEach((link) => {
+      link.style.display = "block";
+    });
+  });
+}
 
 
